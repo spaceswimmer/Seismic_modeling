3c4d8a94aac42135b9049095a99858b9468024ff
spaceswimmer <spaceswimmer69@gmail.com>
2024-07-25 23:09:23 +0300

    fixing problems with branches

--- venv/lib/python3.12/site-packages/jupyterhub/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import base, login, metrics, pages
-from .base import *  # noqa
-from .login import *  # noqa
-
-default_handlers = []
-for mod in (base, pages, login, metrics):
-    default_handlers.extend(mod.default_handlers)
